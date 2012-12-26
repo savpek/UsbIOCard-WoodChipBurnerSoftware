af364b9cffa9adf35699b211e855793a57dee87b
Pekka Savolainen <savolainen.pekka@gmail.com>
2012-12-27 00:59:28 +0200

    Fixed brainfarts in exception and enabled true/false handling.

--- ControlPageRuntime.py
+++ ControlPageRuntime.py
@@ -14,19 +14,22 @@ burner.Delay = 3
 burner.FanTime = 1
 burner.ScrewTime = 1
 
-burner.FanTerminalName = "2.T30"
+burner.FanTerminalName = "2.T0"
 burner.ScrewTerminalName = "2.T1"
 
 class Runner(threading.Thread):
     exceptionMsg = ""
+    Enabled = False
 
     def run(self):
         while True:
+            time.sleep(0.5)
             try:
-                burner.Execute()
+                if self.Enabled:
+                    burner.Execute()
+                    self.exceptionMsg = ""
             except Exception as ex:
                 self.exceptionMsg = ex
-
 
 worker = Runner()
 worker.start()
@@ -36,8 +39,15 @@ def hello_world():
     burner.Delay = float(request.args.get('delay', burner.Delay))
     burner.ScrewTime = float(request.args.get('screwTime', burner.ScrewTime))
     burner.FanTime = float(request.args.get('fanTime', burner.FanTime))
+    worker.Enabled = request.args.get('enabled', worker.Enabled) in ("True", True)
 
-    return render_template('index.html', delay = burner.Delay, fanTime = burner.FanTime, screwTime = burner.ScrewTime, status = worker.exceptionMsg, exceptionMessages = worker.exceptionMsg)
+    return render_template('index.html',
+        delay = burner.Delay,
+        fanTime = burner.FanTime,
+        screwTime = burner.ScrewTime,
+        status = "Status",
+        exceptionMessages = worker.exceptionMsg,
+        enabled = worker.Enabled)
 
 if __name__ == '__main__':
     #app.debug = True
