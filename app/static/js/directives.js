d7ef9353716ea4b3056ec09d12a9db2a067c203c
savpek <savolainen.pekka@gmail.com>
2013-06-08 13:44:23 +0300

    First working version of toggle button directive 'JIIHHAAA'

--- app/static/js/directives.js
+++ app/static/js/directives.js
@@ -11,14 +11,31 @@ directives.helloWorld = function () {
         };
 };
 
-directives.fundooRating = function () {
+directives.toggleButton = function () {
     return {
-      restrict: 'A',
-      link: function (scope, elem, attrs) {
-        console.log("Recognized the fundoo-rating directive usage");
-      }
+        restrict: 'E',
+        template: '<input type="button" ng-click="toggle()" class="{{buttonClass}}">ToggleButton</input>',
+        scope: {},
+        replace: false,
+        controller: ['$scope', '$element', '$attrs', '$transclude', function ($scope, $element, $attrs, $transclude) {
+            $scope.toggleValue = false;
+            $scope.buttonClass = "btn btn-success";
+        }],
+        link: function (scope, elem, attrs) {
+            scope.$watch('toggleValue', function (oldValue, newValue) {
+                if(newValue) {
+                    scope.buttonClass = "btn btn-success";
+                }
+                else {
+                    scope.buttonClass = "btn btn-failure";
+                }
+            });
+
+            scope.toggle = function () {
+                scope.toggleValue = !scope.toggleValue;
+            };
+        }
     };
 };
 
-/* global burnerApp */
 burnerApp.directive(directives);
