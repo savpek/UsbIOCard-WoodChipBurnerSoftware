da0e0949cdc178a79a5f888edbc10f2f30aa1d07
Savpek <savolainen.pekka@gmail.com>
2014-05-10 19:55:32 +0300

    Tested errors handled correctly at io card.

--- WebApp/tests/serialportapi.tests.js
+++ WebApp/tests/serialportapi.tests.js
@@ -1,11 +1,12 @@
 var assert = require("assert");
 
-var burnerPortApi = function(port, error, result) {
+var burnerPortApi = function(port) {
     var commands = {
         "FAN_ENABLE" : "SET 2.T2 HIGH",
         "FAN_DISABLE" : "SET 2.T2 LOW",
         "SCREW_ENABLE" : "SET 2.T1 HIGH",
-        "SCREW_DISABLE" : "SET 2.T1 LOW"
+        "SCREW_DISABLE" : "SET 2.T1 LOW",
+        "READ_LIGHTMETER" : "READ SOMETHING"
     };
 
     var self = this;
@@ -17,16 +18,33 @@ var burnerPortApi = function(port, error, result) {
         parser: serialPort.parsers.readline("\n")
     }, false);
 
-    self.call = function(command) {
+    self.call = function(command, error, result) {
         port.open(function(){
+            if(!commands[command]) {
+                console.log("Invalid parameter given for call, returning.");
+                error("Invalid command parameter for IO card, invalid command was'" + command + "'");
+                return;
+            }
+
             port.on('data', function (data) {
                 console.log('Received from IO-card: ' + data);
 
-                var response = { 'command' : command, 'response': data }
-                result(response)
+                if(result) {
+                    var response = { 'command' : command, 'response': data.replace('\r','').replace('\n',''), 'isEcho':false };
+
+                    if(data.indexOf(commands[command]) > -1) {
+                        response.isEcho = true;
+                    }
+
+                    console.log("Response:", response);
+                    result(response)
+                }
+                else {
+                    console.log("Result function not defined, nothing returned.")
+                }
             });
 
-            console.log('Writing to IO-card: ' + commands[command])
+            console.log('Writing to IO-card: ' + commands[command]);
             port.write(commands[command]+"\n", function(err, result) {
                 if(err) {
                     error(err)
@@ -38,17 +56,22 @@ var burnerPortApi = function(port, error, result) {
 };
 
 describe('Testers: IO card connectivity', function() {
-    it('Enable screw relay.', function (done) {
-        var port = burnerPortApi("COM3", function(err) {
-            done();
-        },
-        function (data) {
-            done();
-        });
+    this.timeout(6000);
 
-        port.call("SCREW_ENABLE");
+    it('Enable screw relay.', function (done) {
+        setTimeout(done, 3000);
+        var port = burnerPortApi("COM3");
+
+        port.call("SCREW_ENABLE", function(err){}, function(res) {});
     });
 
-    it('Read light meter value.', function() {
+    it('Invalid command should cause error.', function(done) {
+        setTimeout(done, 3000);
+        var port = burnerPortApi("COM3");
+
+        port.call("INVALID_COMMAND_HERE", function(err) {
+            console.log("Error received: " + err);
+        }, function(result){
+        });
     });
 });
