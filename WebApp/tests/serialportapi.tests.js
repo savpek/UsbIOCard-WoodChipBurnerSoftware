b2a275b7ef8d2cf4ee26752a8bbd932ab9e7ada1
Savpek <savolainen.pekka@gmail.com>
2014-05-11 13:43:25 +0300

    Faced terribly problems with drivers, first call works but any later call will say that port is closed.

--- WebApp/tests/serialportapi.tests.js
+++ WebApp/tests/serialportapi.tests.js
@@ -1,77 +1,94 @@
-var assert = require("assert");
-
-var burnerPortApi = function(port) {
+var burnerPortApi = (function() {
     var commands = {
         "FAN_ENABLE" : "SET 2.T2 HIGH",
         "FAN_DISABLE" : "SET 2.T2 LOW",
         "SCREW_ENABLE" : "SET 2.T1 HIGH",
         "SCREW_DISABLE" : "SET 2.T1 LOW",
-        "READ_LIGHTMETER" : "READ SOMETHING"
+        "READ_LIGHTMETER" : "ADC 7.T0.ADC0"
     };
 
-    var self = this;
+    var that = this;
 
-    var serialPort = require("serialport");
-    var driver = serialPort.SerialPort;
+    var responseCallBack = function(response) {};
+    var errorCallBack = function(error) {};
 
-    var port = new driver("COM3", { baudrate: 9600,
-        parser: serialPort.parsers.readline("\n")
+    var serialPortFactory = require("serialport");
+
+    var serialPort = new serialPortFactory.SerialPort("COM3", { baudrate: 9600,
+        parser: serialPortFactory.parsers.readline("\n")
     }, false);
 
-    self.call = function(command, error, result) {
-        port.open(function(){
-            if(!commands[command]) {
-                console.log("Invalid parameter given for call, returning.");
-                error("Invalid command parameter for IO card, invalid command was'" + command + "'");
-                return;
-            }
+    that.on = function(action, callback) {
+        switch (action) {
+            case 'error':
+                errorCallBack = callback;
+                break;
+            case 'response':
+                responseCallBack = callback;
+                break;
+            default:
+                console.log("Called on method with invalid action switch.");
+                throw "Called on method with invalid action switch.";
+        }
+    };
 
-            port.on('data', function (data) {
+    serialPort.on('open', function() {
+        console.log("Serial port opened.");
+
+        serialPort.on('close', function() {
+            console.log("connection closed.")
+        });
+
+        serialPort.on('error', function(err) {
+            console.log("vitunvittu." + err);
+            throw "eivitunvittu " + err;
+        });
+    });
+
+    that.write = function(command) {
+        if(!commands[command]) {
+            console.log("Invalid parameter given for call, returning.");
+            errorCallBack("Invalid command parameter for IO card, invalid command was '" + command + "'");
+            return;
+        }
+
+        serialPort.open(function() {
+            serialPort.on('data', function (data) {
                 console.log('Received from IO-card: ' + data);
 
-                if(result) {
-                    var response = { 'command' : command, 'response': data.replace('\r','').replace('\n',''), 'isEcho':false };
+                var response = { 'command': command, 'response': data.replace('\r', '').replace('\n', ''), 'isEcho': false };
 
-                    if(data.indexOf(commands[command]) > -1) {
-                        response.isEcho = true;
-                    }
+                if (data.indexOf(commands[command]) > -1) {
+                    response.isEcho = true;
+                }
 
-                    console.log("Response:", response);
-                    result(response)
-                }
-                else {
-                    console.log("Result function not defined, nothing returned.")
-                }
+                console.log("Response: ", response);
+                responseCallBack(response)
             });
 
             console.log('Writing to IO-card: ' + commands[command]);
-            port.write(commands[command]+"\n", function(err, result) {
-                if(err) {
-                    error(err)
-                }
-            })
+            serialPort.write(commands[command] + "\n");
         });
-    }
-    return self;
-};
+    };
+
+    return that;
+})();
 
 describe('Testers: IO card connectivity', function() {
-    this.timeout(6000);
+    this.timeout(9000);
 
     it('Enable screw relay.', function (done) {
         setTimeout(done, 3000);
-        var port = burnerPortApi("COM3");
+        burnerPortApi.write("SCREW_ENABLE");
+    });
 
-        port.call("SCREW_ENABLE", function(err){}, function(res) {});
+    it('Reading lightmeter returns value.', function(done) {
+        setTimeout(done, 3000);
+        burnerPortApi.write("READ_LIGHTMETER");
     });
 
     it('Invalid command should cause error.', function(done) {
         setTimeout(done, 3000);
-        var port = burnerPortApi("COM3");
-
-        port.call("INVALID_COMMAND_HERE", function(err) {
-            console.log("Error received: " + err);
-        }, function(result){
-        });
+        burnerPortApi.write("INVALID_COMMAND_HERE");
     });
 });
